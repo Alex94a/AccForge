5ace8436988266d1ac6e1138318896341914d21f
Qwerty <Alex1994andr@protonmail.com>
2025-01-09 18:04:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ export const task = {
 export const smsService = {
     name: '5sim',
     youtubeChannel: {
-        apiKey: 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDkxODU5NzIsImlhdCI6MTcxNzY0OTk3MiwicmF5IjoiZDI1NWEwMmRhMDZkZjdmNzgwYTQ5NGY2ZGViMTA3MTgiLCJzdWIiOjIzMjE2NjF9.H3nHY-NAIELz0KA-Q2m5Efpw9_ASohZ4EUbJbNFT6Qdf5DsBwIXO_BMcxwOV3RHaWoMzA_le7Vi50bovjo3Q0UNFIj0I9dGOXhSlFV-oPUo54oIDSOWRSrR32x_64e8n5hgqsVsu4DfE-KX_oYW7ZPywjDTKG4kPky7hCqtueFbYCF8VcGC8r7ABST6SuwzxQhlQEuzlcO8QEp-UEp-cNAmg47SPiKgGRUfeBX57tGNCCm0gegd9p43daNe1DwbAv4l55rddtcWNTslEuDtlCSc7rrhB3JZ5VfGUHtklwoqznpCE47H7on09Wi0Psmm6-SjLN_JFfi_hM3T2PQFB0g',
+        apiKey: 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJle1HAiOjE3NDkxODU5NzIsImlhdCI6MTcxNzY0OTk3MiwicmF5IjoiZDI1NWEwMmRhMDZkZjdmNzgwYTQ5NGY2ZGViMTA3MTgiLCJzdWIiOjIzMjE2NjF9.H3nHY-NAIELz0KA-Q2m5Efpw9_ASohZ4EUbJbNFT6Qdf5DsBwIXO_BMcxwOV3RHaWoMzA_le7Vi50bovjo3Q0UNFIj0I9dGOXhSlFV-oPUo54oIDSOWRSrR32x_64e8n5hgqsVsu4DfE-KX_oYW7ZPywjDTKG4kPky7hCqtueFbYCF8VcGC8r7ABST6SuwzxQhlQEuzlcO8QEp-UEp-cNAmg47SPiKgGRUfeBX57tGNCCm0gegd9p43daNe1DwbAv4l55rddtcWNTslEuDtlCSc7rrhB3JZ5VfGUHtklwoqznpCE47H7on09Wi0Psmm6-SjLN_JFfi_hM3T2PQFB0g',
         country: 'india',
         maxPrice: 6
     }
